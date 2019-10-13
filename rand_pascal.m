30cbd54d0795ec44dbc37c70f74cbd41d1c4ebfe
camillabarreto <42206341+camillabarreto@users.noreply.github.com>
2019-10-13 17:09:00 -0300

    Create rand_pascal.m

---
+++ rand_pascal.m
@@ -0,0 +1,10 @@
+function N = rand_pascal(k,p)
+    N = rand(1) < p;    
+    if k > N 
+        if N == 1
+            N = 1 + rand_pascal(k-1,p); 
+        else
+            N = 1 + rand_pascal(k,p); 
+        end
+    end
+return
